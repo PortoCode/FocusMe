04e2174466ee821f61048f5de56a307a2c024755
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-04-24 08:38:31 -0300

    Initial Commit

---
+++ FocusMe/FocusMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusMeApp.swift
+//  FocusMe
+//
+//  Created by Rodrigo Porto on 24/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
