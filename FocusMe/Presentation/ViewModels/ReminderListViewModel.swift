e5a2008fb3a9478effcb0cc734f03e3bd0ecd2c8
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-04-24 23:24:11 -0300

    feat: add ReminderListViewModel

---
+++ FocusMe/Presentation/ViewModels/ReminderListViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  ReminderListViewModel.swift
+//  FocusMe
+//
+//  Created by Rodrigo Porto on 24/04/25.
+//
+
+import Foundation
+import Combine
+
+final class ReminderListViewModel {
+    @Published var reminders: [Reminder] = []
+    
+    private let fetchRemindersUseCase: FetchRemindersUseCase
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(fetchRemindersUseCase: FetchRemindersUseCase) {
+        self.fetchRemindersUseCase = fetchRemindersUseCase
+        loadReminders()
+    }
+    
+    func loadReminders() {
+        fetchRemindersUseCase.execute()
+            .receive(on: DispatchQueue.main)
+            .assign(to: &$reminders)
+    }
+}
