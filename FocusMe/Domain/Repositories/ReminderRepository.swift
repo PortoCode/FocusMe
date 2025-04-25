f6e7bedd6b79e16f538c04aa8327044f96af1fd7
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-04-24 22:43:12 -0300

    feat: add reminder repository protocol

---
+++ FocusMe/Domain/Repositories/ReminderRepository.swift
@@ -0,0 +1,14 @@
+//
+//  ReminderRepository.swift
+//  FocusMe
+//
+//  Created by Rodrigo Porto on 24/04/25.
+//
+
+import Combine
+
+protocol ReminderRepository {
+    func fetchReminders() -> AnyPublisher<[Reminder], Never>
+    func addReminder(_ reminder: Reminder)
+    func removeReminder(_ reminder: Reminder)
+}
