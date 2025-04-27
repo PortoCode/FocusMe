1223d4ad0ce333cf3e94a07b3a822a45d786d3dd
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-04-27 00:41:30 -0300

    feat: add factory for ReminderListView

---
+++ FocusMe/Presentation/Factories/ReminderListFactory.swift
@@ -0,0 +1,21 @@
+//
+//  ReminderListFactory.swift
+//  FocusMe
+//
+//  Created by Rodrigo Porto on 27/04/25.
+//
+
+import Foundation
+
+enum ReminderListFactory {
+    static func make() -> ReminderListView {
+        let repository = MockReminderRepository()
+        let fetchUseCase = FetchRemindersUseCase(repository: repository)
+        let addUseCase = AddReminderUseCase(repository: repository)
+        let viewModel = ReminderListViewModel(
+            fetchRemindersUseCase: fetchUseCase,
+            addReminderUseCase: addUseCase
+        )
+        return ReminderListView(viewModel: viewModel)
+    }
+}
