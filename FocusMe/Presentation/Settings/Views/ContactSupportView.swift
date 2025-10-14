8ca2d98abd1222579400ca622decc86674d9e780
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-10-14 20:22:13 -0300

    refactor: simplify contact links by creating a reusable method for generating links with accessibility support

--- FocusMe/Presentation/Settings/Views/ContactSupportView.swift
+++ FocusMe/Presentation/Settings/Views/ContactSupportView.swift
@@ -11,23 +11,33 @@ struct ContactSupportView: View {
     var body: some View {
         List {
             Section(header: Text("Contact")) {
-                Link(destination: ContactConstants.linkedInURL) {
-                    Label("LinkedIn", systemImage: "link.circle")
-                        .accessibilityLabel("Open LinkedIn profile of Rodrigo Porto")
-                }
-                Link(destination: ContactConstants.githubURL) {
-                    Label("GitHub", systemImage: "chevron.left.slash.chevron.right")
-                        .accessibilityLabel("Open GitHub profile of Rodrigo Porto")
-                }
-                Link(destination: ContactConstants.mailtoURL) {
-                    Label("Email", systemImage: "envelope")
-                        .accessibilityLabel("Send an email to Rodrigo Porto")
-                }
+                contactLink(title: "LinkedIn",
+                            url: ContactConstants.linkedInURL,
+                            icon: "link.circle",
+                            accessibilityText: "Open LinkedIn profile of Rodrigo Porto")
+                
+                contactLink(title: "GitHub",
+                            url: ContactConstants.githubURL,
+                            icon: "chevron.left.slash.chevron.right",
+                            accessibilityText: "Open GitHub profile of Rodrigo Porto")
+                
+                contactLink(title: "Email",
+                            url: ContactConstants.mailtoURL,
+                            icon: "envelope",
+                            accessibilityText: "Send an email to Rodrigo Porto")
             }
         }
         .navigationTitle("Contact Support")
     }
+    
+    private func contactLink(title: String, url: URL, icon: String, accessibilityText: String) -> some View {
+        Link(destination: url) {
+            Label(title, systemImage: icon)
+                .accessibilityLabel(accessibilityText)
+        }
+    }
 }
+
 
 #Preview {
     ContactSupportView()
